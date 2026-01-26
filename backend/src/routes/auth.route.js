7683ad6f006f365a84aaea447af4c4f0a9371f9f
Rohan Roy <roy.rohan14378@gmail.com>
2026-01-26 14:47:36 +0530

    Save current progress

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -3,7 +3,9 @@ import { checkAuth, login, logout, signup, updateProfile } from "../controllers/
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
-
+const app = express();
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
